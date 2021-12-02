6e4361658ba6a8ed6625bc9ed797388b7151db74
Danielle Bencze <daniellebencze@live.com>
2021-12-02 09:37:45 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const thankYouCards = []
+
+function writeCards(thankYouCards, event) {
+    for(let i = 0; i < thankYouCards.length; i++) {
+        thankYouCards.splice(i, 1, `Thank you, ${thankYouCards[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+writeCards(["Lisa", "Kaitlin", "Jan"], "birthday");
+
+function countDown(x) {
+    while (x >= 0) {
+        console.log(x);
+        x--
+    }
+}
